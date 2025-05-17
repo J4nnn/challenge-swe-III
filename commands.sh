f433cfa336435ba2f7d24da5fb7bd16e703d9176
Janer Vega <janervega399@gmail.com>
2025-05-16 22:29:16 -0500

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -5,9 +5,8 @@ sudo microk8s enable dns ingress
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.9.4/deploy/static/provider/cloud/deploy.yaml
 
 # Generar certificado TLS
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
--keyout app1.key -out app1.crt -subj \
-"/CN=app1.example.com/O=app1"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout app1.key -out app1.crt -subj "/CN=app1.example.com/O=app1"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout app2.key -out app2.crt -subj "/CN=app2.example.com/O=app2"
 
 # Crear secret para el certificado TLS
 kubectl create secret tls app1-tls --cert=app1.crt --key=app1.key
@@ -15,6 +14,7 @@ kubectl create secret tls app2-tls --cert=app2.crt --key=app2.key
 
 # Desplegar la aplicacion
 sudo microk8s kubectl apply -f app1.yml
+sudo microk8s kubectl apply -f app2.yml
 
 # Crear el ingress para la aplicacion
 sudo microk8s kubectl apply -f ingress.yml
